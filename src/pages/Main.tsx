507dbd07dd468fafba824bd59e222aaa5fb5ca38
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-02-29 22:12:44 +0200

    add layout

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import "scss/_main.page.scss"
 
 export default function Main() {
     return (
-        <div>
-            Home
+        <div className='container'>
+            <div className="content">
+                <p className="paragraph">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Proin vitae erat vitae diam gravida ornare.
+                    Nunc ac mi non tortor porta rhoncus quis vitae augue.
+                    In ac sapien sit amet massa lacinia congue commodo a nisl.
+                    Vestibulum auctor nisl a purus auctor fermentum.
+                    Mauris purus mauris, pretium vel ex vel, ultricies mattis urna.
+                    Sed non risus ultrices, blandit sem nec, auctor sem.
+                    Nulla pretium porta neque eget ornare.
+                </p>
+                <p className='paragraph'>
+                    Proin ullamcorper accumsan nisi at tempor.
+                    Etiam lacinia maximus enim, ac consequat sapien faucibus in.
+                    Nullam in venenatis risus. Morbi nisi erat, hendrerit et tincidunt vel, suscipit fermentum elit.
+                    Suspendisse maximus ipsum non faucibus placerat.
+                    Donec augue augue, imperdiet sed sodales sed, blandit a dui.
+                    Proin mollis, turpis a molestie laoreet, lectus nisl malesuada lorem, quis pellentesque mauris est venenatis tortor.
+                    Pellentesque aliquam urna et tristique mattis.
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+                    Mauris dignissim aliquet mauris, molestie dapibus neque dictum tincidunt.
+                    Proin vulputate lectus ac placerat tincidunt.
+                </p>
+                <p className='paragraph'>
+                    Vestibulum ullamcorper varius erat et euismod.
+                    Nullam at turpis erat. Aliquam erat volutpat.
+                    Sed varius mi nunc. Suspendisse at dolor in tortor placerat lacinia.
+                    Maecenas malesuada ornare tellus, at pharetra justo mattis non.
+                    Nunc eu aliquet enim.
+                    Praesent et felis ultrices, laoreet nisl et, placerat tellus.
+                    Aenean aliquet, sapien a luctus condimentum, turpis mi ultricies turpis, vitae eleifend justo magna laoreet arcu.
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                    Donec nec neque pellentesque, eleifend ex id, rhoncus ante.
+                    Duis enim nisl, tincidunt ac nunc at, rutrum scelerisque leo. Vestibulum et rutrum nibh.
+                    Etiam in velit vitae turpis pretium tempor at a orci. Proin sit amet mollis ex.
+                </p>
+            </div>
         </div>
     )
 }
