02a5e2736a29eccb89a2d2a6469332d57d683eac
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-02-29 22:12:14 +0200

    add layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoPokemon from "img/LogoPokemon.png"
 import "scss/_header.scss"
 
-export default function Header() {
+const Header:React.FC = () => {
     return (
         <div className="header">
-            <p className="text">header</p>
+            <div className="header__logo">
+                <div className="header__logo-container">
+                    <img className="header__logo-img" src={LogoPokemon} alt="logo" />
+                </div>
+            </div>
+            <ul className="header__menu">
+                <NavLink className="list__item list__item-page" to=''>
+                    Main
+                </NavLink>
+                <NavLink className="list__item list__item-page" to='/About'>
+                    About
+                </NavLink>
+                <NavLink className="list__item list__item-page" to='/CardsPokemonInfo'>
+                    Cards Pokemon Info
+                </NavLink>
+            </ul>
         </div>
     )
 }
+export default Header
