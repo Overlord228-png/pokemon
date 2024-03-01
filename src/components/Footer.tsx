a99825418c236ccefbf59b2eabcdfcf5863710d2
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-03-01 09:53:12 +0200

    add content

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Footer() {
+    return(
+        <div className="content">
+            <h1 className="title">Добро пожаловать на сайт о покемонах!</h1>
+            <p className="description">Здесь вы найдете всю информацию о ваших любимых покемонах, их характеристиках, типах, тренерах и многое другое.</p>
+            <p className="copyright"> 2024 Pokemon Company. All Rights Reserved.</p>
+        </div>
+    )
+};
