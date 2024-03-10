3055bf80c8729b0835e54079a58a9c2bd5ebf5a2
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-03-10 16:30:42 +0200

    corect interface

--- src/pages/CardsPokemonInfo.tsx
+++ src/pages/CardsPokemonInfo.tsx
@@ -2,67 +2,67 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "scss/_cards.pokemon.Info.scss";
 import PokemonSearchInput from "components/PokemonSearchInput";
+import PokemonCard from "components/PokemonCard"
 
 interface Pokemon {
     name: string;
     url: string;
+    sprites: {
+        front_default: string;
+    };
+    attack: number,
+    strength: number,
+    agility: number,
+    health: number,
+    defense:number
 }
 
 function CardsPokemonInfo() {
-    // Состояние для списка покемонов, текущей страницы и общего количества страниц
     const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
     const [currentPage, setCurrentPage] = useState<number>(1);
     const [pokemonPerPage] = useState<number>(5);
     const [totalPages, setTotalPages] = useState<number>(1);
     const [searchQuery, setSearchQuery] = useState<string>('');
     const [error, setError] = useState<string | null>(null);
-    const [validQuery, setValidQuery] = useState<boolean>(true); 
+    const [validQuery, setValidQuery] = useState<boolean>(true);
+    const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
 
     useEffect(() => {
-        // Функция для получения списка покемонов с API
         const fetchPokemonList = async () => {
             try {
-                // Получаем список покемонов с указанием смещения и лимита
                 const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${(currentPage - 1) * pokemonPerPage}&limit=${pokemonPerPage}`);
-                // Устанавливаем полученный список покемонов
                 setPokemonList(response.data.results);
 
-                // Вычисляем общее количество страниц на основе общего количества покемонов
                 const totalCount = response.data.count;
                 setTotalPages(Math.ceil(totalCount / pokemonPerPage));
-                setError(null); // Сброс ошибки при успешном запросе
+                setError(null);
             } catch (error) {
                 console.error('Error fetching Pokemon:', error);
-                setError('Failed to fetch Pokemon. Please try again.'); // Установка ошибки при неудачном запросе
+                setError('Failed to fetch Pokemon. Please try again.');
             }
         };
 
-        fetchPokemonList(); // Вызываем функцию получения списка покемонов
+        fetchPokemonList();
     }, [currentPage, pokemonPerPage]);
 
-    // Функция для перехода на следующую страницу
     const nextPage = () => {
         if (currentPage < totalPages) {
             setCurrentPage(currentPage + 1);
         }
     };
 
-    // Функция для перехода на предыдущую страницу
     const prevPage = () => {
         if (currentPage > 1) {
             setCurrentPage(currentPage - 1);
         }
     };
 
-    // Функция для установки текущей страницы
     const setPage = (page: number) => {
         setCurrentPage(page);
     };
 
-    // Функция для отрисовки пагинации
     const renderPagination = () => {
-        const pagesToShow = 6; // Количество отображаемых страниц
-
+        const pagesToShow = 6;
         const pages = [];
         let startPage = 1;
         let endPage = totalPages;
@@ -107,15 +107,12 @@ function CardsPokemonInfo() {
         return pages;
     };
 
-    // Функция для фильтрации покемонов по имени
     const filteredPokemon = pokemonList.filter(pokemon => pokemon.name.toLowerCase().includes(searchQuery.toLowerCase()));
 
-    // Обработчик изменения значения в поле поиска
     const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchQuery(event.target.value);
     };
 
-    // Обработчик поиска покемонов
     const handleSearch = (searchQuery: string) => {
         setSearchQuery(searchQuery);
         if (searchQuery.length > 20) {
@@ -125,16 +122,31 @@ function CardsPokemonInfo() {
         }
     };
 
+    const fetchPokemonDetails = async (url: string) => {
+        try {
+            const response = await axios.get(url);
+            setSelectedPokemon(response.data);
+        } catch (error) {
+            console.error('Error, fetching Pokemon::', error);
+            setError('Failed to fetch Pokemon details. Please try again.');
+        }
+    };
+
     return (
-        <div className="pokemon-info-container">
-            {error && <div className="error-message">{error}</div>}
-            <h1>Pokemon List</h1>
+        <div className="pokemon__info-container">
+            {error && <div className="error__message">{error}</div>}
+            <h1 className="pokemon-list-title">Pokemon List</h1>
+
             <PokemonSearchInput value={searchQuery} onChange={handleSearchChange} onSearch={handleSearch} />
-            {!validQuery && <div className="error-message">Search query is too long. Maximum length is 20 characters.</div>}
-            {filteredPokemon.length === 0 && <div className="error-message">No Pokemon found.</div>}
-            <ul>
+
+            {!validQuery && <div className="error__message">Search query is too long. Maximum length is 20 characters.</div>}
+            {filteredPokemon.length === 0 && <div className="error__message">No Pokemon found.</div>}
+
+            <ul className="pokemon__list">
                 {filteredPokemon.map((pokemon: Pokemon, index: number) => (
-                    <li key={index}>{pokemon.name}</li>
+                    <li key={index} onClick={() => fetchPokemonDetails(pokemon.url)} className="pokemon__item">
+                        {pokemon.name}
+                    </li>
                 ))}
             </ul>
             <div className="pagination">
@@ -142,6 +154,12 @@ function CardsPokemonInfo() {
                 {renderPagination()}
                 <button onClick={nextPage} disabled={currentPage === totalPages}>&gt;</button>
             </div>
+
+            {selectedPokemon && (
+                <div className="pokemon__details">
+                    <PokemonCard pokemon={selectedPokemon} />
+                </div>
+            )}
         </div>
     );
 }
